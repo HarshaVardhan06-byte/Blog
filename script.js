c9a5d6cd9d5c4e6c7d62f7ff989118e88ca63020
HarshaVardhan06-byte <harsharaj4906@gmail.com>
2026-04-28 22:02:22 +0530

    Added Google Analytics

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ function searchBlogs(){
    card.style.display=card.innerText.toLowerCase().includes(input)?'block':'none';
  });
 }
+
+
+window.onload = function () {
+    const randomNum = Math.floor(Math.random() * 1000);
+
+    const img = document.getElementById("randomBlogImage");
+
+    if (img) {
+        img.src = `https://picsum.photos/900/500?random=${randomNum}`;
+    }
+};
